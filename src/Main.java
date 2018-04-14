7f23001ba2623ef3585fddd7613ef82a54b8da5d
Vova Pilipenko <vovapilipenko@MacBook-Pro-Vova-2.local>
2018-04-14 22:07:54 +0200

    merge

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 1000; i++){
-            System.out.println("PYLYP pidor");
+        for(int i = 0; i < 1000; i++) {
+            System.out.println("Vova pidor");
+        }
+    }
+
+    public void volodiaPidor(boolean isItTrue){
+        if(isItTrue){
+            System.out.println("Tak i znal");
+        }else{
+            System.out.println("Vse ravno pidor");
         }
     }
     public static void azaza(){
