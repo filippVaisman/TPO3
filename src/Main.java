761d2f0ec1f1050bc86050bf25775170c2279baa
Vova Pilipenko <vovapilipenko@MacBook-Pro-Vova-2.local>
2018-04-14 21:30:28 +0200

    pylyp pidor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("123");
+        for(int i = 0; i < 1000; i++){
+            System.out.println("PYLYP pidor");
+        }
     }
 }
