aa3ffc458b7942f3932fc4965d93024e6c33941c
filippVaisman <vaisman1626@gmail.com>
2018-04-14 22:23:04 +0200

    Reset

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,4 @@
 
 public class Main {
-    public static void main(String[] args) {
-        for(int i = 0; i < 1000; i++) {
-            System.out.println("Vova pidor");
-        }
-    }
     
-
-    public void volodiaPidor(boolean isItTrue){
-        if(isItTrue){
-            System.out.println("Tak i znal");
-        }else{
-            System.out.println("Vse ravno pidor");
-        }
-    }
-    public static void azaza(){
-        System.out.println("oryyyyyy");
-    }
-
-    public void moreFunctions(){
-        System.out.println("do nothing");
-    }
-
 }
