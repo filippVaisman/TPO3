72e9f9abb9b71952ad59047271fd73a8cd0ed388
filippVaisman <vaisman1626@gmail.com>
2018-04-15 18:48:40 +0200

    Json Parser

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,12 +1,29 @@
 package application;
 
+import application.Net.SimpleRequest;
+
+import java.io.IOException;
+
 public class Client {
-    final private int port = 1235;
-    final private int portMainServer = 1234;
     //TODO: make ip ok
     final private String ipMainServer = "localhost";
-
+    final private int port = 1235;
+    final private int portMainServer = 1234;
     private String [] languages;
 
+    public void sendRequestTomMainServer(String json){
+        try {
+            SimpleRequest requestToMain = new SimpleRequest(ipMainServer,portMainServer,this.getClass());
+            requestToMain.sendRequest(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+
+    public void translate(String word){
+        //TODO: make json here
+        sendRequestTomMainServer("");
+    }
 }
---
+++ src/application/Json/JsonFormater.java
@@ -0,0 +1,7 @@
+package application.Json;
+
+/**
+ * Created by Philipp on 15.04.2018.
+ */
+public class JsonFormater {
+}
