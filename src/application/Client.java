11c2f7957fc166ddfbb76364d6d67c56fb40f9ac
filippVaisman <vaisman1626@gmail.com>
2018-04-15 17:39:49 +0200

    Ports.txt added

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,4 +1,12 @@
-package application.Servers;
+package application;
 
 public class Client {
+    final private int port = 1235;
+    final private int portMainServer = 1234;
+    //TODO: make ip ok
+    final private String ipMainServer = "localhost";
+
+    private String [] languages;
+
+
 }
--- src/application/Servers/MainServer.java
+++ src/application/Servers/MainServer.java
@@ -1,7 +1,5 @@
 package application.Servers;
 
-/**
- * Created by Philipp on 15.04.2018.
- */
+
 public class MainServer {
 }
